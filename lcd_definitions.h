c2fe7a0d347849332620e614322caf94b296ac86
Hasan Karimi <destructor.iri@gmail.com>
2020-10-23 15:13:18 +0330

    feat: add character LCD

---
+++ lcd_definitions.h
@@ -0,0 +1,11 @@
+#define LCD_WRAP_LINES	1
+
+#define LCD_PORT		PORTD
+
+#define LCD_DATA0_PIN	3
+#define LCD_DATA1_PIN	4
+#define LCD_DATA2_PIN	5
+#define LCD_DATA3_PIN	6
+#define LCD_RS_PIN		0
+#define LCD_RW_PIN		1
+#define LCD_E_PIN		2
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
 
 int main(void) {
+
+	sei();
+
+	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+	lcd_clrscr();
 
 	while(1) {
 
